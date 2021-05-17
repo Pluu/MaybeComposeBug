b13c015e3cc921e5ebbe1e0b6ad85ec6f5346acf
pluulove <pluulove@gmail.com>
2021-05-18 00:42:55 +0900

    cleanup sample

--- app/src/main/java/com/pluu/accompanistbug/GlideBasicSample.kt
+++ app/src/main/java/com/pluu/accompanistbug/GlideBasicSample.kt
@@ -1,6 +1,7 @@
 package com.pluu.accompanistbug
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
@@ -10,15 +11,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.FragmentActivity
 import com.google.accompanist.glide.rememberGlidePainter
 
-class GlideBasicSample : FragmentActivity() {
+class GlideBasicSample : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            // Not working, first vsync
             AccompanistSampleTheme {
                 Sample()
             }
@@ -30,7 +28,7 @@ private const val NumberItems = 60
 
 @OptIn(ExperimentalStdlibApi::class)
 @Composable
-fun Sample() {
+private fun Sample() {
     LazyColumn {
         items(NumberItems) { index ->
             val painter = rememberGlidePainter(
